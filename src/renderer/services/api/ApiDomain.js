31de6429ee6b910e1810ded359c2c5a8b9fb7772
Maguas Tupaguas <mauricioaznar94@gmail.com>
2018-07-04 11:11:48 -0500

    Build bugs.

--- src/renderer/services/api/ApiDomain.js
+++ src/renderer/services/api/ApiDomain.js
@@ -1,2 +1,3 @@
-const ApiDomain = 'http://innoserver.test/api/'
+const ApiDomain = (process.env.NODE_ENV === 'development') ? 'http://innoserver.test/api/' : 'http://206.189.228.240/api/'
+
 export default ApiDomain
