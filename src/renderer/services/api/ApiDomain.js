9a6e2679a6f7acf3b4123798f30a068d7666be99
Maguas Tupaguas <mauricioaznar94@gmail.com>
2018-07-11 17:07:25 -0500

    Css fix

--- src/renderer/services/api/ApiDomain.js
+++ src/renderer/services/api/ApiDomain.js
@@ -1,3 +1,3 @@
-const ApiDomain = (process.env.NODE_ENV === 'development') ? 'http://monsrealserver.test/api/' : 'http://192.168.100.125/api/'
+const ApiDomain = (process.env.NODE_ENV === 'development') ? 'http://monsrealserver.test/api/' : 'http://104.236.36.18/api/'
 
 export default ApiDomain
