9efa63b185f4257dc291e6ccf742a87540c8343b
mauricioaznar <31492123+mauricioaznar@users.noreply.github.com>
2018-07-13 15:11:03 -0500

    Update ApiDomain.js

--- src/renderer/services/api/ApiDomain.js
+++ src/renderer/services/api/ApiDomain.js
@@ -1,3 +1,3 @@
-const ApiDomain = (process.env.NODE_ENV === 'development') ? 'http://monsrealserver.test/api/' : 'http://192.168.100.25/api/'
+const ApiDomain = (process.env.NODE_ENV === 'development') ? 'http://monsrealserver.test/api/' : 'http://192.168.100.125/api/'
 
 export default ApiDomain
